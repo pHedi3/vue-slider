6ada8077ae7495a9fd51a57dded918b047ddbf2d
pHedi3 <federico.gualdi3@gmail.com>
2021-06-25 16:03:16 +0200

    inizio es

---
+++ js/script.js
@@ -0,0 +1,14 @@
+new Vue(
+    {
+        el: '#app',
+        data: {
+            img: [
+                'img/1.jpg',
+                'img/463135-new-girl-new-girl.jpg',
+                'img/0477956dd0d298457a284be0e68cfd6f110f1f08fa43796b59a869f677aa39bc._RI_V_TTW_.jpg',
+                'img/la-casa-di-carta-4-netflix_70447f24.jpg',
+                'img/Tenebre-e-Ossa.jpg'
+            ]
+        }
+    }
+)
