a47ee8e3367798842c654906fb66dc413b18082e
pHedi3 <federico.gualdi3@gmail.com>
2021-06-25 16:43:28 +0200

    fine prima parte

--- js/script.js
+++ js/script.js
@@ -8,7 +8,36 @@ new Vue(
                 'img/0477956dd0d298457a284be0e68cfd6f110f1f08fa43796b59a869f677aa39bc._RI_V_TTW_.jpg',
                 'img/la-casa-di-carta-4-netflix_70447f24.jpg',
                 'img/Tenebre-e-Ossa.jpg'
-            ]
+            ],
+            imgCunter: 0
+        },
+        methods: {
+            next: function() {
+                if (this.imgCunter === this.img.length - 1) {
+                   this.imgCunter = 0
+                } else {
+                    this.imgCunter++
+                }
+            },
+            befor: function() {
+                if (this.imgCunter === 0) {
+                    this.imgCunter = this.img.length - 1
+                } else {
+                    this.imgCunter--
+                }
+            },
+            pointer: function(index) {
+                if( index === this.imgCunter) {
+                    return 'pointer' + index
+                }
+            }
+
+        },
+        created() {
+            setInterval(() => {
+                this.next()
+            }, 2000)
         }
+
     }
 )
